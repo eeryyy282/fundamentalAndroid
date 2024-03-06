cb3cde9a5502844b0016c2295da8b5b9dd8226c3
eeryyy282 <airiagustus82@gmail.com>
2024-03-07 03:58:47 +0700

    First push to GitHub

---
+++ app/src/main/java/com/example/fundamentalandroid/MainActivity.kt
@@ -0,0 +1,36 @@
+package com.example.fundamentalandroid
+
+import android.content.Intent
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.fundamentalandroid.fragment.FlexibleFragment
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        val btnMoveToFlexibleFragment: Button = findViewById(R.id.buttonMoveToFlexibleFragment)
+        btnMoveToFlexibleFragment.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        when(v?.id) {
+            R.id.buttonMoveToFlexibleFragment -> {
+                val moveToFlexibleFragment = Intent(this@MainActivity, FlexibleFragment::class.java)
+                startActivity(moveToFlexibleFragment)
+            }
+        }
+    }
+}
