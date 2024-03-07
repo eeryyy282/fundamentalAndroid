6a68b3c64547ed54fb6dc32cd958ea3493c91931
eeryyy282 <airiagustus82@gmail.com>
2024-03-07 14:26:22 +0700

    add a flexible fragment (category fragment) in one activity

--- app/src/main/java/com/example/fundamentalandroid/learnfragment/fragment/HomeFragment.kt
+++ app/src/main/java/com/example/fundamentalandroid/learnfragment/fragment/HomeFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import com.example.fundamentalandroid.R
 
 class HomeFragment : Fragment(), View.OnClickListener {
@@ -17,8 +18,23 @@ class HomeFragment : Fragment(), View.OnClickListener {
         return inflater.inflate(R.layout.fragment_home, container, false)
     }
 
-    override fun onClick(v: View?) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
 
+        val btnCategory: Button = view.findViewById(R.id.btn_category)
+        btnCategory.setOnClickListener(this)
+    }
+
+    override fun onClick(v: View?) {
+        if(v?.id == R.id.btn_category) {
+            val categoryFragment = CategoryFragment()
+            val fragmentManager = parentFragmentManager
+            fragmentManager.beginTransaction().apply {
+                replace(R.id.frame_container, categoryFragment, CategoryFragment::class.java.simpleName)
+                addToBackStack(null)
+                commit()
+            }
+        }
     }
 
 }
